7250b31e3d631cdbbf977fc240fc0360e7e6c1f8
Papershine <hilarylau119@gmail.com>
2023-11-19 11:07:45 -0800

    commit before npm

--- scripts/content.js
+++ scripts/content.js
@@ -8,8 +8,14 @@ document.querySelectorAll('[data-text]').forEach(element => {
   
 function parse(e) {
   const currentInput = e.target.value;
-  highlight(currentInput, "fuck")
-  console.log(currentInput);
+  var innerHTML = e.target.innerHTML;
+  console.log(innerHTML)
+  var index = innerHTML.indexOf("fuck");
+  if (index >= 0) {
+    console.log("detected word")
+    innerHTML = innerHTML.substring(0,index) + "<span class='highlight' style='background-color: yellow;'>" + innerHTML.substring(index,index+"fuck".length) + "</span>" + innerHTML.substring(index + "fuck".length);
+    e.currentTarget.innerHTML = innerHTML;
+  }
 }
 
 function highlight(innerHTML, text) {
