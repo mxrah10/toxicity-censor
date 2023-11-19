760481eb6701d6a7cc875eed468fc696ef124c74
Papershine <hilarylau119@gmail.com>
2023-11-19 10:52:14 -0800

    add css changes:

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,23 @@
 document.querySelectorAll('input[type="text"], textarea').forEach(element => {
-    element.addEventListener('input', function(event) {
-      const currentInput = event.target.value;
-      alert("Please remember to inhibit your emotions, we think you might be saying something negative or toxic.")
-    });
-  });
+  element.addEventListener('input', parse);
+});
+
+document.querySelectorAll('[data-text]').forEach(element => {
+  element.addEventListener('change', parse);
+})
   
+function parse(e) {
+  const currentInput = e.target.value;
+  highlight(currentInput, "fuck")
+  console.log(currentInput);
+}
+
+function highlight(innerHTML, text) {
+  //var innerHTML = e.currentTarget.innerHTML;
+  var index = innerHTML.indexOf(text);
+  if (index >= 0) {
+    console.log("detected word")
+    innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+    e.currentTarget.innerHTML = innerHTML;
+  }
+}
