60f1098c306a6d188d4745c0d058155dcee08c48
Home Acc. <minori.poedjokerto@gmail.com>
2023-11-19 15:12:28 -0800

    parsing uninclusive and suggestions

--- parse.js
+++ parse.js
@@ -1,15 +1,20 @@
-let example = "test *text text text text* test test";
-let parsedText;
-
-function parseProfanity(inputText){
-    var parsedText = inputText.substring(
-        inputText.indexOf("*") + 1, 
-        inputText.lastIndexOf("*")
+function parseUninclusive(inputText){
+    var parsedUninclusive = inputText.substring(
+        inputText.indexOf("(") + 1,
+        inputText.lastIndexOf(")")
     );
 
-    const ProfanityArray = parsedText.split(" ");
-    
-    return ProfanityArray;
+    const UninclusiveArray = parsedUninclusive.split(" ");
+    return UninclusiveArray;
 }
 
-console.log(parseProfanity(example));
+function parseSuggestion(inputText){
+    var parsedSuggestions = inputText.substring(
+        inputText.indexOf("[") + 1,
+        inputText.lastIndexOf("]")
+    )
+
+    const SuggestionsArray = parsedSuggestions.split(" ");
+    return SuggestionsArray;
+}
+
