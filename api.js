49c56873e169c0be8b44b75eb9d9907a2ee847e1
Home Acc. <minori.poedjokerto@gmail.com>
2023-11-19 16:18:22 -0800

    integration testing

--- api.js
+++ api.js
@@ -1,39 +1,48 @@
 require('dotenv').config();
 
-let prompt = null;
 
-const apiKey = process.env.OPENAI_API_KEY; //use your own api key >:(
+let prompt = null; 
+
+const apiKey = null; //use your own api key >:(
 const url = 'https://api.openai.com/v1/chat/completions';
 
-function setPrompt(text){
-    prompt = text
+function setPrompt(userTextInput){
+    prompt = userTextInput
+
 }
 
-const queryData = {
+function CallAPI(){
+    const queryData = {
     model: "gpt-3.5-turbo", 
     messages: [
-        { "role": "user", "content": "Give me the non-inclusive or toxic or negative words by enclosing them in (), then the suggested words to replace them in [] from the following text without explaining anything. Here is the text:" + prompt}
+        { "role": "user", "content": "Give me the non-inclusive or toxic or negative words by enclosing them in (), then the suggested words to replace them in [] from the following text without explaining anything. Here is the text:" + prompt }
     ]
-};
+    };
 
-const resp = null;
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-    },
-    body: JSON.stringify(queryData)
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
+    const resp = null;
 
-    if (data.choices && data.choices.length > 0 && data.choices[0].message) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify(queryData)
+    })
 
-        console.log("Response from OpenAI:", data.choices[0].message.content);
-    } else {
-        console.log("No response content found.");
-    }
-})
-.catch(error => console.error('Error:', error));
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        if (data.choices && data.choices.length > 0 && data.choices[0].message) {
+            //console.log("Response from OpenAI:", data.choices[0].message.content);
+            return data.choices[0].message.content;
+
+        } else {
+            //console.log("No response content found.");
+            return null;
+        }
+    })
+
+    .catch(error => console.error('Error:', error));
+}
--- scripts/content.js
+++ scripts/content.js
@@ -1,83 +1,88 @@
 // Query for textareas
-chrome.storage.local.get('enabled', function (data) {
-  if (!data.enabled) {
-    return;
-  }
-  var textareaFound = false;
-  document.querySelectorAll('input[type="text"], textarea, [contenteditable=true]').forEach(element => {
+chrome.storage.local.get('enabled', function(data) {
+    if (!data.enabled) {
+      return; 
+    }
+    var textareaFound = false;
+    document.querySelectorAll('input[type="text"], textarea, [contenteditable=true]').forEach(element => {
     textareaFound = true;
     console.log("There is an element " + element.className);
     element.addEventListener('input', parse);
-  });
+    });
 
-  function parse(e) {
+    function parse(e) {
     console.log("parse fired");
     const currentInput = e.target.value;
     showPopup(currentInput);
     console.log(currentInput);
-  }
+    }
 
-  // Query for spans (reddit, twitter)
-  if (!textareaFound) {
+    // Query for spans (reddit, twitter)
+    if (!textareaFound) {
     var observer = new MutationObserver(function (mutations) {
-      mutations.forEach((mutation) => {
+        mutations.forEach((mutation) => {
         console.log("subtree changed");
         console.log(mutation.target);
         //showPopup(mutation.target.wholeText);
         showPopup(mutation.target.wholeText);
-      });
+        });
     });
     var config = { childList: true, subtree: true, characterData: true };
 
     setTimeout(() => {
-      queryPoll();
+        queryPoll();
     }, 1000)
 
     function queryPoll() {
-      var found = false;
-      console.log("poll");
-      document.querySelectorAll('[data-text], [data-lexical-text]').forEach(element => {
+        var found = false;
+        console.log("poll");
+        document.querySelectorAll('[data-text], [data-lexical-text]').forEach(element => {
         found = true;
         console.log("There is an element " + element.className);
         element.addEventListener('keyup', parse);
         observer.observe(element, config);
-      })
-      setTimeout(() => {
+        })
+        setTimeout(() => {
         queryPoll();
-      }, 1000)
+        }, 1000)
     }
-  }
-
-  // highlight text given html
-  function highlight(innerHTML, text) {
-    var innerHTML = e.currentTarget.innerHTML;
-    var index = innerHTML.indexOf(text);
-    if (index >= 0) {
-      console.log("detected word")
-      innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
-      e.currentTarget.innerHTML = innerHTML;
     }
-  }
 
-  // show the popup
-  function showPopup(text) {
-    var hasPopup = document.getElementsByClassName('detoxify-popup');
-    if (hasPopup.length > 0) {
-      document.getElementById('detoxify-content').innerHTML = text;
-    } else {
-      document.body.insertAdjacentHTML('beforeend', `
-        <div class="detoxify-popup">
-            <div class="detoxify-titlebar">
-              <span class="detoxify-title">DETOXIFY</span>
-              <span class="detoxify-close" id="detoxify-close">X</span>
+    // highlight text given html
+    function highlight(innerHTML, badWord) {
+        var innerHTML = e.currentTarget.innerHTML;
+        var index = innerHTML.indexOf(badWord);
+        if (index >= 0) {
+            console.log("detected word")
+            innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + badWord.length) + "</span>" + innerHTML.substring(index + badWord.length);
+            e.currentTarget.innerHTML = innerHTML;
+        }
+    }
+
+    // show the popup
+    function showPopup(text) {
+        var hasPopup = document.getElementsByClassName('detoxify-popup');
+        
+        setPrompt(text);
+        API_Text = CallAPI(); 
+        
+        if (API_Text != null){
+            parseUninclusive(API_Text); // Creates an array of uninclusive words called UninclusiveArray[]
+        }
+
+        for (let words = 0; words < UninclusiveArray; words++) {
+            highlight(text, UninclusiveArray[words])
+        }
+
+        if (hasPopup.length > 0) {
+            document.getElementById('detoxify-content').innerHTML = text;
+        } else {
+            document.body.insertAdjacentHTML('beforeend', `
+            <div class="detoxify-popup">
+                <p class="detoxify-title">DETOXIFY</p>
+                <span id="detoxify-content">` + text + `</span>
             </div>
-            <span id="detoxify-content">` + text + `</span>
-        </div>
-      `);
-      document.getElementById("detoxify-close").onclick = () => {
-        document.getElementsByClassName('detoxify-popup')[0].outerHTML = '';
-      }
+            `);
+        }
     }
-  }
-
 });
